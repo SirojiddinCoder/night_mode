69296546cc9b8305a850578eb7061b2f42f5f7ae
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2022-01-12 00:04:48 +0500

    night-mode full

--- night-mode/src/App.js
+++ night-mode/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+// import * as sss from 'styled-components';
+// console.log(sss, 'styled');
+
+export const lightTheme = {
+  body: '#fff ',
+  fontColor: '#000',
+};
+
+export const darkTheme = {
+  body: '#000',
+  fontColor: '#fff',
+};
+
+export const Globalstyles = createGlobalStyle`
+body{
+    background-color: ${(props) => {
+      console.log(props, 'props');
+
+      return props.theme.body;
+    }};
+}
+
+`;
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') :  setTheme('light');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <Globalstyles theme={theme === 'light' ? lightTheme : darkTheme} />
+      <styledApp>
+        Sirojiddin
+        <button onClick={themeToggler}>darkMode</button>
+      </styledApp>
+    </ThemeProvider>
   );
 }
 
